3b8a746803ed72e7856d3b0d57b7bc80db135e33
manisha220 <manishachaubey634@gmail.com>
2026-06-27 16:36:10 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,29 @@
+console.log("abc");
+
+name="manisha"; //string
+console.log(name);
+
+price=99.99; //float
+console.log(price);
+
+x=null; //null value
+console.log(x);
+
+y=undefined; //undefined value
+console.log(y);
+
+isFollow=false; //boolean value
+console.log(isFollow);
+
+//JavaScript is a dynamically typed language
+
+//let, var and const
+let age=24;
+let fullName="manisha chaubey";
+let price=1000;
+
+var vage=24;
+var vname="manisha";
+var vprice=1000;
+
+const c_age=30;
---
+++ op_and_condit.js
@@ -0,0 +1,60 @@
+//Operators
+//ARITHMETIC
+let a=5;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b); //exponentiation
+console.log(a++); //post increment
+console.log(++a); //pre increment
+console.log(b--); //post decrement
+console.log(--b); //pre decrement
+
+//ASSIGNMENT
+let c=3;
+console.log(c);
+c+=2;
+console.log(c);
+c-=1;
+console.log(c);
+c*=3;
+console.log(c);
+c%=2;
+console.log(c);
+c**=2;
+console.log(c);
+
+//COMPARISION
+a=5;
+b="5";
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+
+//LOGICAL
+console.log(a==b && a==5);
+console.log(a===b || a==5);
+console.log(a!=b);
+
+//CONDITIONAL STATEMENTS
+/* works same as c++*/
+
+ //TERNARY OPERATORS
+ a=5;
+ b=2;
+ a>b?console.log("TRUE"):console.log("FALSE");
+
+//SWITCH
+/*works same as c++*/ 
+
+//PROMPT
+let name=prompt("Enter Name:");
+console.log(name);
